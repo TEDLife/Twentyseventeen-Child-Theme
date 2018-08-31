e915ebcd8d09016c83d73bb2954a6f98513f9c6a
tedlife <zhongguojym@gmail.com>
2018-08-31 09:28:41 +0800

    fix: no posts layout in homepage

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
  */
 
 get_header(); ?>
-		
+
 <div class="wrap">
 	<?php if ( is_home() && ! is_front_page() ) : ?>
 		<header class="page-header">
@@ -24,31 +24,30 @@ get_header(); ?>
 		</header>
 	<?php else : ?>
 	<header class="page-header">
-		<h2 class="page-title">最新资源</h2>
+		<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
 	</header>
 	<?php endif; ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<div class="row">
-			<?php
-			if ( have_posts() ) :
 
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
+			<?php if ( have_posts() ) : ?>
+			<div class="row">
+				<?php while ( have_posts() ) : the_post();
 
 					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
+						* Include the Post-Format-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						*/
 					get_template_part( 'template-parts/post/content', 'home' );
 
-				endwhile;
-			?>
+				endwhile; ?>
 			</div>
-			<?php 
+
+
+			<?php
 					the_posts_pagination( array(
 					'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
 					'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
@@ -61,7 +60,7 @@ get_header(); ?>
 
 			endif;
 			?>
-			
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
