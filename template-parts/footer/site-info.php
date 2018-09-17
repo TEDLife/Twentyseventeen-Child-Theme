b5bb106df8b63211ecfbb7d3dfd5f99ad979eb83
tedlife <zhongguojym@gmail.com>
2018-09-17 14:45:04 +0800

    fix: script position

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -12,9 +12,3 @@
 <div class="site-info">
 	<p class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
 </div><!-- .site-info -->
-
-<script type="text/javascript">
-    var ouo_token = 'aWwJKXhn';
-    var exclude_domains = ['ouo.io'];
-</script>
-<script src="//cdn.ouo.io/js/full-page-script.js"></script>
